e065eaf8bb8ef2972b9e4205d194218fc2c50285
Teo03 <39939408+Teodor03@users.noreply.github.com>
2021-01-31 10:22:00 +0200

    Update dinic_max_flow.cpp

--- Max Flow/dinic_max_flow.cpp
+++ Max Flow/dinic_max_flow.cpp
@@ -4,128 +4,129 @@
 #include<algorithm>
 using namespace std;
 
-const int Max_V=10001;
-const int Max_cap=999999999;
+const int Max_N=1000000;
+const int Inf = 999999999;
 
+int n,m,so,si;
 struct edge{
-int to;
-int weight;
-edge * opp;
+int to,we,opp;
 };
 
-int n,m,source,sink;
-vector<edge> graph[Max_V];
+vector<edge> gr [Max_N];
 
-void build_edge(int f,int s,int w){
-edge a;
-edge b;
-a.to=s;
-a.weight=w;
-graph[f].push_back(a);
-b.to=f;
-b.weight=0;
-graph[s].push_back(b);
-graph[f][graph[f].size()-1].opp=&graph[s][graph[s].size()-1];
-graph[s][graph[s].size()-1].opp=&graph[f][graph[f].size()-1];
-return;
+void add_edge(int a,int b,int c){
+    edge fo,bac;
+    fo.to=b;
+    fo.we=c;
+    bac.to=a;
+    bac.we=0;
+    gr[a].push_back(fo);
+    gr[b].push_back(bac);
+    gr[a][gr[a].size()-1].opp=gr[b].size()-1;
+    gr[b][gr[b].size()-1].opp=gr[a].size()-1;
+    return;
 }
 
-bool visited[Max_V];
-int lev [Max_V];
+bool vis [Max_N];
+int lev [Max_N];
+int par [Max_N];
+int num_son [Max_N];
 
-bool BFS(){
-queue<int> q;
-q.push(source);
-fill(visited,visited+n,false);
-visited[source]=true;
-lev[source]=0;
-int curr;
-while(!q.empty()){
-    curr=q.front();
-    q.pop();
-    for(int i=0;i<graph[curr].size();i++){
-        if(!visited[graph[curr][i].to] and graph[curr][i].weight!=0){
-            visited[graph[curr][i].to]=true;
-            lev[graph[curr][i].to]=lev[curr]+1;
-            q.push(graph[curr][i].to);
+void reset_level(){
+    for(int i=0;i<n;i++){
+        lev[i]=-1;
+    }
+}
+
+void reset_vis(){
+    for(int i=0;i<n;i++){
+        vis[i]=false;
+    }
+}
+
+bool bfs(){
+    queue<int> q;
+    reset_level();
+    lev[so]=0;
+    q.push(so);
+    int curr;
+    while(!q.empty()){
+        curr=q.front();
+        q.pop();
+        for(int i=0;i<gr[curr].size();i++){
+            if(lev[gr[curr][i].to]==-1 and gr[curr][i].we!=0){
+                lev[gr[curr][i].to]=lev[curr]+1;
+                q.push(gr[curr][i].to);
+            }
+        }
+    }
+//    for(int i=0;i<n;i++){
+//        cout<<i+1<<" : "<<lev[i]<<endl;
+//    }
+    return lev[si]!=-1;
+}
+
+void dfs(int curr){
+    for(int i=0;i<gr[curr].size();i++){
+        if( (!vis[gr[curr][i].to]) and gr[curr][i].we>0 and (lev[gr[curr][i].to] == lev[curr]+1 ) ){
+            vis[gr[curr][i].to]=true;
+            par[gr[curr][i].to]=curr;
+            num_son[gr[curr][i].to]=i;
+            dfs(gr[curr][i].to);
         }
     }
 }
-return visited[sink];
+
+bool dfs_driver(){
+    reset_vis();
+    vis[so]=true;
+    par[so]=-1;
+    dfs(so);
+    return vis[si];
 }
 
-int prev [Max_V];
-int num_son [Max_V];
-int min_to [Max_V];
+int Dinic(){
+    int res=0,c,_min;
+    while(bfs()){
 
-void DFS(int curr){
-for(int i=0;i<graph[curr].size();i++){
-    if(!visited[graph[curr][i].to] and graph[curr][i].weight!=0 and (lev[graph[curr][i].to]-lev[curr])==1){
-        visited[graph[curr][i].to]=true;
-        prev[graph[curr][i].to]=curr;
-        num_son[graph[curr][i].to]=i;
-        min_to[graph[curr][i].to]=min(min_to[curr],graph[curr][i].weight);
-        DFS(graph[curr][i].to);
-    }
-}
-return;
-}
-
-bool call_DFS(){
-prev[source]=-1;
-num_son[source]=-1;
-min_to[source]=Max_cap;
-fill(visited,visited+n,false);
-visited[source]=true;
-DFS(source);
-return visited[sink];
-}
-
-int max_flow_dinic(){
-int result=0;
-int curr,min_pipe;
-while(BFS()){
-    while(call_DFS()){
-        min_pipe=min_to[sink];
-        curr=sink;
-        while(prev[curr]!=-1){
-            graph[prev[curr]][num_son[curr]].weight-=min_pipe;
-            (* graph[prev[curr]][num_son[curr]].opp).weight+=min_pipe;
-            curr=prev[curr];
+        while(dfs_driver()){
+            _min=Inf;
+            c=si;
+            while(par[c]!=-1){
+                _min=min(_min,gr[par[c]][num_son[c]].we);
+                c=par[c];
+            }
+            c=si;
+            while(par[c]!=-1){
+                gr[par[c]][num_son[c]].we-=_min;
+                gr[c][gr[par[c]][num_son[c]].opp].we+=_min;
+                c=par[c];
+            }
+            res+=_min;
         }
-        result+=min_pipe;
+
+    }
+
+    return res;
+}
+
+void input(){
+    ios_base::sync_with_stdio(false);
+    cin>>n>>m>>so>>si;
+    so--;
+    si--;
+    int a,b,c;
+    for(int i=0;i<m;i++){
+        cin>>a>>b>>c;
+        a--;
+        b--;
+        add_edge(a,b,c);
     }
 }
-return result;
-}
-
-/*
-Example:
-
-6 8 1 6
-1 2 11
-1 3 12
-3 2 1
-2 4 12
-3 5 11
-5 4 7
-5 6 4
-4 6 19
-
-*/
 
 int main(){
-ios_base::sync_with_stdio(false);
-cin>>n>>m>>source>>sink;
-source--;
-sink--;
-int a,b,c;
-for(int i=0;i<m;i++){
-    cin>>a>>b>>c;
-    a--;
-    b--;
-    build_edge(a,b,c);
-}
-cout<<max_flow_dinic();
-return 0;
+    ios_base::sync_with_stdio(false);
+    input();
+    cout<<"Result : "<<Dinic()<<endl;
+    return 0;
 }
